fd1becd688a735d9a18af7793475c757cb8ebb47
bensaadat <bensaadat.amine@gmail.com>
2022-12-14 11:29:55 +0100

    add gym progress add desc and is main

--- app/Http/Controllers/GymsController.php
+++ app/Http/Controllers/GymsController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Files;
 use App\Models\Gyms;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -47,44 +48,67 @@ class GymsController extends Controller{
     public function store(Request $request)
     {
         $user_id = auth()->user()->id;
-            $this->validate(
+        //validation form 
+          $this->validate(
                 $request, 
                     [
                         'gym_name' => 'required',
                         'gym_address' => 'required',
-                        'gym_phone' => 'required',
-                        'profile_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                        'gym_phone' => 'regex:/[0-9]/',
+                        'profile_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
                     ],
-                  
                     [
                         'gym_name.required' => __('translation.require_gym_name'),
                         'gym_address.required' => __('translation.require_gym_address'),
-                        'gym_phone.required' => __('translation.require_gym_phone'),
+                        'gym_phone.regex' => __('translation.require_gym_phone'),
                         'profile_image' =>   __('translation.file_not_autorized')
                     ],
                 );
 
-               
-            $file = $request->file('profile_image') ;
-
-            if($file = $request->hasFile('profile_image')) {
-                    $file = $request->file('profile_image') ;
-                    $fileName = $file->getClientOriginalName() ;
-                    $destinationPath = public_path().'/images' ;
-                    $extension = $request->file('profile_image')->extension();
-
-                    
-                    $file->move($destinationPath,$fileName);
-                    dd($fileName, $destinationPath, $extension);
-                    // return redirect('/uploadfile');
-            }
-            //dd($file);
+            
+           
+        // save gym in gym table
         $gym= new Gyms();
-            $gym->name = $request['gym_name'];
-            $gym->phone = $request['gym_phone'];
-            $gym->address = $request['gym_address'];
-            $gym->created_by = $user_id;
+        $gym->name = $request['gym_name'];
+        $gym->phone = $request['gym_phone'];
+        $gym->address = $request['gym_address'];
+        $gym->desc = $request['gym_desc'];
+        $gym->is_main = $request['is_main'];
+        $gym->created_by = $user_id;
         $gym->save();
+        
+        // save gym profile image
+        $file = $request->file('profile_image');
+        if($file = $request->hasFile('profile_image')) {
+
+            // file data 
+            $file = $request->file('profile_image') ;
+            $fileName = time().rand(100,999).preg_replace('/\s+/', '', $file->getClientOriginalName());
+            $destinationPath = public_path().'/assets/images/gyms/' ;
+            $extension = $request->file('profile_image')->extension();
+
+            // save gym image in file table
+            $files_table= new Files();
+            $files_table->name = $fileName;
+            $files_table->ext = $extension;
+            $files_table->type = 'profile';
+            $files_table->entitiy_id = $gym->id;   
+            $files_table->save();
+
+            // move file in dericory
+            $file->move($destinationPath,$fileName);
+
+        }else{
+             // save gym image in file table
+             $files_table= new Files();
+             $files_table->name = "Simply-Gym-Logo.png";
+             $files_table->ext = "png";
+             $files_table->type = 'profile';
+             $files_table->entitiy_id = $gym->id;   
+             $files_table->save();
+        }
+
+
         return redirect('/gyms');
     }
 
--- resources/views/gym/gym_create.blade.php
+++ resources/views/gym/gym_create.blade.php
@@ -18,7 +18,7 @@
                         <div class="mb-3">
                             <div class="mb-3">
                                 <label class="form-label" for="gym-name-input">Name</label>
-                                <input type="text" class="form-control" name="gym_name" id="gym-name-input" value="" placeholder="Enter gym name" required>
+                                <input type="text" class="form-control" name="gym_name" id="gym-name-input" value="{{ old('gym_name') }}" placeholder="Enter gym name" required>
                                 @error('gym_name')
                                     <div class="invalid-feedback" style="display: block;">{{ $message }}</div>
                                 @enderror
@@ -27,7 +27,7 @@
                         <div class="mb-3">
                             <div class="mb-3">
                                 <label class="form-label" for="product-title-input">Address</label>
-                                <input type="text" class="form-control" name="gym_address" id="gym-address-input" value="" placeholder="Enter address of your gym" required>
+                                <input type="text" class="form-control" name="gym_address" id="gym-address-input" value="{{ old('gym_address') }}" placeholder="Enter address of your gym" required>
                                 @error('gym_address')
                                     <div class="invalid-feedback" style="display: block;">{{ $message }}</div>
                                 @enderror
@@ -36,23 +36,17 @@
                         <div class="mb-3">
                             <div class="mb-3">
                                 <label class="form-label" for="product-title-input">Phone</label>
-                                <input type="text" class="form-control" name="gym_phone" id="gym-phone-input" value="" placeholder="Enter phone of your gym" required>
+                                <input type="text" class="form-control" name="gym_phone" id="gym-phone-input" value="{{ old('gym_phone') }}" placeholder="Enter phone of your gym" required>
                                 @error('gym_phone')
                                     <div class="invalid-feedback" style="display: block;">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-                        <div>
-                            <label>Product Description</label>
 
-                            <div id="ckeditor-classic">
-                                <p>Tommy Hilfiger men striped pink sweatshirt. Crafted with cotton. Material composition is 100% organic cotton. This is one of the world’s leading designer lifestyle brands and is internationally recognized for celebrating the essence of classic American cool style, featuring preppy with a twist designs.</p>
-                                <ul>
-                                    <li>Full Sleeve</li>
-                                    <li>Cotton</li>
-                                    <li>All Sizes available</li>
-                                    <li>4 Different Color</li>
-                                </ul>
+                        <div class="mb-3">
+                            <div class="mb-3">
+                                <label class="form-label" for="product-title-input">Description</label>
+                                <textarea name="gym_desc" class="form-control bg-light border-light" id="gym-desc-input" rows="3" placeholder="Enter descreption her"></textarea>
                             </div>
                         </div>
                     </div>
@@ -138,105 +132,6 @@
                 </div>
                 <!-- end card -->
 
-                <div class="card">
-                    <div class="card-header">
-                        <ul class="nav nav-tabs-custom card-header-tabs border-bottom-0" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" data-bs-toggle="tab" href="#addproduct-general-info"
-                                    role="tab">
-                                    General Info
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="tab" href="#addproduct-metadata"
-                                    role="tab">
-                                    Meta Data
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- end card header -->
-                    <div class="card-body">
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="addproduct-general-info" role="tabpanel">
-                                <div class="mb-3">
-                                    <label class="form-label" for="manufacturer-name-input">Manufacturer Name</label>
-                                    <input type="text" class="form-control" id="manufacturer-name-input"
-                                        placeholder="Enter manufacturer name">
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-3 col-sm-6">
-                                        <div class="mb-3">
-                                            <label class="form-label" for="stocks-input">Stocks</label>
-                                            <input type="text" class="form-control" id="stocks-input" placeholder="Stocks" required>
-                                            <div class="invalid-feedback">Please Enter a product stocks.</div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-3 col-sm-6">
-                                        <div class="mb-3">
-                                            <label class="form-label" for="product-price-input">Price</label>
-                                            <div class="input-group has-validation mb-3">
-                                                <span class="input-group-text" id="product-price-addon">$</span>
-                                                <input type="text" class="form-control" id="product-price-input" placeholder="Enter price" aria-label="Price" aria-describedby="product-price-addon" required>
-                                                <div class="invalid-feedback">Please Enter a product price.</div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-3 col-sm-6">
-                                        <div class="mb-3">
-                                            <label class="form-label" for="product-discount-input">Discount</label>
-                                            <div class="input-group mb-3">
-                                                <span class="input-group-text" id="product-discount-addon">%</span>
-                                                <input type="text" class="form-control" id="product-discount-input" placeholder="Enter discount" aria-label="discount" aria-describedby="product-discount-addon">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-3 col-sm-6">
-                                        <div class="mb-3">
-                                            <label class="form-label" for="orders-input">Orders</label>
-                                            <input type="text" class="form-control" id="orders-input" placeholder="Orders" required>
-                                            <div class="invalid-feedback">Please Enter a product orders.</div>
-                                        </div>
-                                    </div>
-                                    <!-- end col -->
-                                </div>
-                                <!-- end row -->
-                            </div>
-                            <!-- end tab-pane -->
-
-                            <div class="tab-pane" id="addproduct-metadata" role="tabpanel">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="mb-3">
-                                            <label class="form-label" for="meta-title-input">Meta title</label>
-                                            <input type="text" class="form-control" placeholder="Enter meta title" id="meta-title-input">
-                                        </div>
-                                    </div>
-                                    <!-- end col -->
-
-                                    <div class="col-lg-6">
-                                        <div class="mb-3">
-                                            <label class="form-label" for="meta-keywords-input">Meta Keywords</label>
-                                            <input type="text" class="form-control" placeholder="Enter meta keywords" id="meta-keywords-input">
-                                        </div>
-                                    </div>
-                                    <!-- end col -->
-                                </div>
-                                <!-- end row -->
-
-                                <div>
-                                    <label class="form-label" for="meta-description-input">Meta Description</label>
-                                    <textarea class="form-control" id="meta-description-input" placeholder="Enter meta description" rows="3"></textarea>
-                                </div>
-                            </div>
-                            <!-- end tab pane -->
-                        </div>
-                        <!-- end tab content -->
-                    </div>
-                    <!-- end card body -->
-                </div>
-                <!-- end card -->
                 <div class="text-end mb-3">
                     <button type="submit" class="btn btn-success w-sm">Submit</button>
                 </div>
@@ -246,96 +141,21 @@
         <div class="col-lg-4">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="card-title mb-0">Publish</h5>
+                    <h5 class="card-title mb-0">Gym statu</h5>
                 </div>
                 <div class="card-body">
                     <div class="mb-3">
-                        <label for="choices-publish-status-input" class="form-label">Status</label>
+                        <label for="choices-is_main" class="form-label">Is main ?</label>
 
-                        <select class="form-select" id="choices-publish-status-input" data-choices data-choices-search-false>
-                            <option value="Published" selected>Published</option>
-                            <option value="Scheduled">Scheduled</option>
-                            <option value="Draft">Draft</option>
-                        </select>
-                    </div>
-
-                    <div>
-                        <label for="choices-publish-visibility-input" class="form-label">Visibility</label>
-                        <select class="form-select" id="choices-publish-visibility-input" data-choices data-choices-search-false>
-                            <option value="Public" selected>Public</option>
-                            <option value="Hidden">Hidden</option>
+                        <select name="is_main" class="form-select" id="choices-is_main">
+                            <option value="1" selected>Is main</option>
+                            <option value="0">Is not main</option>
                         </select>
                     </div>
                 </div>
                 <!-- end card body -->
             </div>
             <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Publish Schedule</h5>
-                </div>
-                <!-- end card body -->
-                <div class="card-body">
-                    <div>
-                        <label for="datepicker-publish-input" class="form-label">Publish Date & Time</label>
-                        <input type="text" id="datepicker-publish-input" class="form-control"
-                            placeholder="Enter publish date" data-provider="flatpickr" data-date-format="d.m.y"
-                            data-enable-time>
-                    </div>
-                </div>
-            </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Product Categories</h5>
-                </div>
-                <div class="card-body">
-                    <p class="text-muted mb-2"> <a href="#" class="float-end text-decoration-underline">Add
-                        New</a>Select product category</p>
-                        <select class="form-select" id="choices-category-input" name="choices-category-input" data-choices data-choices-search-false>
-                            <option value="Appliances">Appliances</option>
-                            <option value="Automotive Accessories">Automotive Accessories</option>
-                            <option value="Electronics">Electronics</option>
-                            <option value="Fashion">Fashion</option>
-                            <option value="Furniture">Furniture</option>
-                            <option value="Grocery">Grocery</option>
-                            <option value="Kids">Kids</option>
-                            <option value="Watches">Watches</option>
-                        </select>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Product Tags</h5>
-                </div>
-                <div class="card-body">
-                    <div class="hstack gap-3 align-items-start">
-                        <div class="flex-grow-1">
-                            <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Enter tags" type="text"
-                        value="Cotton" />
-                        </div>
-                    </div>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Product Short Description</h5>
-                </div>
-                <div class="card-body">
-                    <p class="text-muted mb-2">Add short description for product</p>
-                    <textarea class="form-control" placeholder="Must enter minimum of a 100 characters" rows="3"></textarea>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
         </div>
     </div>
     <!-- end row -->
