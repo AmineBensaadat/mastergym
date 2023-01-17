2c9c0c8ec2ef5b84a4bb1f4c99a28aa2a489af99
bensaadat <bensaadat.amine@gmail.com>
2023-01-17 16:31:15 +0100

    change logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/logo_1.png')}}">
     @include('layouts.head-css')
 </head>
 
