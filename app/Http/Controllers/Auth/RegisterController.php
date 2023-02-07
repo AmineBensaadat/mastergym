bded5c755d9ac2d9752b5c05759b476ea8aaf83f
Amine <bensaadat.amine@gmail.com>
2023-02-07 21:12:44 +0100

    update sigin in module

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,7 +71,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $account_id = $this->userRepository->countMaxAccountId();
-       
+       //dd($data);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -124,7 +124,7 @@
                                 alt="Header Avatar">
                             <span class="text-start ms-xl-2">
                                 <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{Auth::user()->name}}</span>
-                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                                {{-- <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span> --}}
                             </span>
                         </span>
                     </button>
