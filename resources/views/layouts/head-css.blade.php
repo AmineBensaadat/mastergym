d064298071fe35c4b519a571204502ecd0817e3f
bensaadat <bensaadat.amine@gmail.com>
2023-02-27 16:25:45 +0100

    fix error arabic

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -4,6 +4,7 @@
 <script src="{{ URL::asset('assets/js/layout.js') }}"></script>
 <!-- Bootstrap Css -->
 <link href="{{ URL::asset('assets/css/bootstrap-rtl.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
 <link href="{{ URL::asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
