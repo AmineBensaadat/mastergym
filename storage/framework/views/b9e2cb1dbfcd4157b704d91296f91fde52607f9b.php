601cd638702b727e223ff297181943ffb8fbb96d
bensaadat <bensaadat.amine@gmail.com>
2023-03-02 12:11:32 +0100

    update login page

--- storage/framework/views/b9e2cb1dbfcd4157b704d91296f91fde52607f9b.php
+++ storage/framework/views/b9e2cb1dbfcd4157b704d91296f91fde52607f9b.php
@@ -108,14 +108,12 @@ unset($__errorArgs, $__bag); ?>
                                     </div>
 
                                     <div class="mt-4 text-center">
-                                        <div class="signin-other-title">
-                                            <h5 class="fs-13 mb-4 title">Sign In with</h5>
+                                        <div class="signin-other-title"> 
+                                            <h5 class="fs-13 mb-4 title">Don't have an account ?</h5>
                                         </div>
                                         <div>
-                                            <button type="button" class="btn btn-primary btn-icon waves-effect waves-light"><i class="ri-facebook-fill fs-16"></i></button>
-                                            <button type="button" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-google-fill fs-16"></i></button>
-                                            <button type="button" class="btn btn-dark btn-icon waves-effect waves-light"><i class="ri-github-fill fs-16"></i></button>
-                                            <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
+                                            <a href="register" class="btn btn-info w-100"> Signup </a>
+
                                         </div>
                                     </div>
                                 </form>
@@ -124,11 +122,6 @@ unset($__errorArgs, $__bag); ?>
                         <!-- end card body -->
                     </div>
                     <!-- end card -->
-
-                    <div class="mt-4 text-center">
-                        <p class="mb-0">Don't have an account ? <a href="register" class="fw-semibold text-primary text-decoration-underline"> Signup </a> </p>
-                    </div>
-
                 </div>
             </div>
             <!-- end row -->
