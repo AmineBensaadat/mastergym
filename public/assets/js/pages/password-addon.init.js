2fb68db613c4fa454cfc6577b568732db86535f8
Amine <bensaadat.amine@gmail.com>
2023-03-18 22:22:25 +0100

    fix show password

--- public/assets/js/pages/password-addon.init.js
+++ public/assets/js/pages/password-addon.init.js
@@ -11,18 +11,13 @@ Contact: CapSolutions@gmail.com
 File: Password addon Js File
 */
 // password addon
-Array.from(document.querySelectorAll("form .auth-pass-inputgroup")).forEach(function (item) {
-  Array.from(item.querySelectorAll(".password-addon")).forEach(function (subitem) {
-    subitem.addEventListener("click", function (event) {
-      var passwordInput = item.querySelector(".password-input");
+$( "form .auth-pass-inputgroup" ).click(function() {
+  if('password' == $('#password-input').attr('type')){
+    $('#password-input').prop('type', 'text');
+  }else{
+      $('#password-input').prop('type', 'password');
+  }
 
-      if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-      } else {
-        passwordInput.type = "password";
-      }
-    });
-  });
 });
 /******/ })()
 ;
