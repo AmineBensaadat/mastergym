b4324ada1ab63d1c691aab46a4f2fc8f40366740
Amine <bensaadat.amine@gmail.com>
2023-03-21 20:35:59 +0000

    git update in dashboard

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -234,7 +234,7 @@ class MembersController extends Controller
                 $sub_array[] = '
                 <div class="d-flex align-items-center">            
                     <div class="flex-shrink-0 ">
-                        <img src="'.(file_exists('assets/images/services/'.$this->filesRepository->getFileByEntityId($row->id, "services","profile")) ? $this->filesRepository->getFileByEntityId($row->id, "services","profile"): 'default.png').'" alt="" class="avatar-xs">
+                        <img src="'.$this->filesRepository->getFileByEntityId($row->service_id, "services","profile").'" alt="" class="avatar-xs">
                     </div>
                     <div class="flex-grow-1 ms-2 name">'.$row->service_name.'</div>            
                 </div>';
@@ -246,7 +246,7 @@ class MembersController extends Controller
                 $sub_array[] = '
                 <div class="d-flex align-items-center">            
                     <div class="flex-shrink-0 ">
-                        <img src="'.(file_exists('assets/images/plans/'.$this->filesRepository->getFileByEntityId($row->id, "plans","profile")) ? $this->filesRepository->getFileByEntityId($row->id, "plans","profile"): 'default.png').'" alt="" class="avatar-xs">
+                        <img src="'.$this->filesRepository->getFileByEntityId($row->plan_id, "plans","profile").'" alt="" class="avatar-xs">
                     </div>
                     <div class="flex-grow-1 ms-2 name">'.$row->plan_name.'</div>            
                 </div>';
