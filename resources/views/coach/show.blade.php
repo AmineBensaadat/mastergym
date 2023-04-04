ae70c1ed19d7b22aa710a85dff5bb118c8ac512d
bensaadat <bensaadat.amine@gmail.com>
2023-04-04 11:12:15 +0000

    coach progrss add full calander

--- resources/views/coach/show.blade.php
+++ resources/views/coach/show.blade.php
@@ -4,8 +4,10 @@
 @endsection
 @section('css')
     <link rel="stylesheet" href="{{ URL::asset('assets/libs/swiper/swiper.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
 @endsection
 @section('content')
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <div class="profile-foreground position-relative mx-n4 mt-n4">
         <div class="profile-wid-bg">
             <img src="{{ URL::asset('assets/images/profile-bg.jpg') }}" alt="" class="profile-wid-img" />
@@ -105,8 +107,7 @@
                             <div class="col-xxl-9">
                                 <div class="card">
                                     <div class="card-body">
-                                        <h5 class="card-title mb-3">About</h5>
-                                        <p></p>
+                                        <div id="calendar"></div>
                                     </div>
                                     <!--end card-body-->
                                 </div><!-- end card -->
@@ -127,7 +128,129 @@
 @endsection
 @section('script')
     <script src="{{ URL::asset('assets/libs/swiper/swiper.min.js') }}"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
     <script src="{{ URL::asset('assets/js/pages/profile.init.js') }}"></script>
     <script src="{{ URL::asset('/assets/js/app.min.js') }}"></script>
+    <script>
+
+        $(document).ready(function () {
+        
+            $.ajaxSetup({
+                headers:{
+                    'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        
+            var calendar = $('#calendar').fullCalendar({
+                editable:true,
+                header:{
+                    left:'prev,next today',
+                    center:'title',
+                    right:'month,agendaWeek,agendaDay'
+                },
+                events:'/coach/show/1',
+                selectable:true,
+                selectHelper: true,
+                select:function(start, end, allDay)
+                {
+                    var title = prompt('Event Title:');
+        
+                    if(title)
+                    {
+                        var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
+        
+                        var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
+        
+                        $.ajax({
+                            url:"/coach/action",
+                            type:"POST",
+                            data:{
+                                title: title,
+                                start: start,
+                                end: end,
+                                type: 'add'
+                            },
+                            success:function(data)
+                            {
+                                calendar.fullCalendar('refetchEvents');
+                                alert("Event Created Successfully");
+                            }
+                        })
+                    }
+                },
+                editable:true,
+                eventResize: function(event, delta)
+                {
+                    var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+                    var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+                    var title = event.title;
+                    var id = event.id;
+                    $.ajax({
+                        url:"/coach/action",
+                        type:"POST",
+                        data:{
+                            title: title,
+                            start: start,
+                            end: end,
+                            id: id,
+                            type: 'update'
+                        },
+                        success:function(response)
+                        {
+                            calendar.fullCalendar('refetchEvents');
+                            alert("Event Updated Successfully");
+                        }
+                    })
+                },
+                eventDrop: function(event, delta)
+                {
+                    var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+                    var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+                    var title = event.title;
+                    var id = event.id;
+                    $.ajax({
+                        url:"/coach/action",
+                        type:"POST",
+                        data:{
+                            title: title,
+                            start: start,
+                            end: end,
+                            id: id,
+                            type: 'update'
+                        },
+                        success:function(response)
+                        {
+                            calendar.fullCalendar('refetchEvents');
+                            alert("Event Updated Successfully");
+                        }
+                    })
+                },
+        
+                eventClick:function(event)
+                {
+                    if(confirm("Are you sure you want to remove it?"))
+                    {
+                        var id = event.id;
+                        $.ajax({
+                            url:"/coach/action",
+                            type:"POST",
+                            data:{
+                                id:id,
+                                type:"delete"
+                            },
+                            success:function(response)
+                            {
+                                calendar.fullCalendar('refetchEvents');
+                                alert("Event Deleted Successfully");
+                            }
+                        })
+                    }
+                }
+            });
+        
+        });
+          
+        </script>
 @endsection
